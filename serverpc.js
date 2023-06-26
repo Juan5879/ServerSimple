19e65df21ee08c742963458e2a903774b43d0b67
Juan5879 <juan5879.2005@gmail.com>
2023-06-26 00:25:59 -0300

    Subido 25/06/2023

--- serverpc.js
+++ serverpc.js
@@ -1,7 +1,35 @@
 const express = require('express');
 const app = express();
+const os = require('os');
 const multer = require('multer');
 const routes = require('./routes/routes');
+
+// Obtener la dirección IP local
+function getLocalIP() {
+  const interfaces = os.networkInterfaces();
+  let ipAddress = null;
+
+  for (const networkInterface of Object.values(interfaces)) {
+    for (const iface of networkInterface) {
+      // Ignorar direcciones IPv6 y direcciones de loopback
+      if (iface.family === 'IPv4' && !iface.internal) {
+        ipAddress = iface.address;
+        break;
+      }
+    }
+
+    if (ipAddress) {
+      break;
+    }
+  }
+
+  return ipAddress;
+}
+
+// Obtener y mostrar la dirección IP local
+const localIP = getLocalIP();
+console.log('IP local:', localIP);
+
 
 // Configuración del motor de plantillas
 app.set('view engine', 'ejs');
@@ -34,5 +62,5 @@ app.post('/upload', upload.single('file'), (req, res) => {
 // Iniciar el servidor
 const port = 3500;
 app.listen(port, () => {
-  console.log(`Servidor en funcionamiento en http://localhost:${port}`);
+  console.log(`Servidor en funcionamiento en http://localhost:${port} \n Desde otro dispositivo accede desde http://${localIP}:${port}`);
 });
